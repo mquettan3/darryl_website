3e279f2b55d471fd5c7d46e76aa4ed5634f4178f
Marcus Quettan <m_quettan@gatech.edu>
2016-06-24 21:43:09 +0000

    Working audio controls and play list.

--- client/views/components/audio_gallery.js
+++ client/views/components/audio_gallery.js
@@ -1,4 +1,4 @@
-var state = "stop";
+var state = "pause";
 var playlist_entries, audio;
 var current_song_id = 0;
 
@@ -14,6 +14,14 @@ function playAudio(){
     $("#button_play i").removeClass("fa fa-play");
     $("#button_play i").addClass("fa fa-pause");
     audio.load();
+    audio.play();
+}
+
+function resumeAudio(){
+    state='resume';
+    $("#button_play").addClass('btn-success');
+    $("#button_play i").removeClass("fa fa-play");
+    $("#button_play i").addClass("fa fa-pause");
     audio.play();
 }
 
@@ -33,6 +41,14 @@ function stopAudio() {
     audio.pause();
     audio.currentTime = 0;
 }
+
+function formatTime(seconds) {
+    minutes = Math.floor(seconds / 60);
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  }
 
 Template.audio_gallery.onCreated(function(){
     // Soundcloud Widget
@@ -56,7 +72,9 @@ Template.audio_gallery.onCreated(function(){
 
     this.audio_location = new ReactiveVar("audio/Yuppppp(s.u.)69bpm.mp3");
     this.current_song_name = new ReactiveVar("N/A");
-
+    this.current_song_time = new ReactiveVar("0:00");
+    this.current_song_duration = new ReactiveVar("0:00");
+    this.current_song_percentage = new ReactiveVar("0");
 });
 
 Template.audio_gallery.onRendered(function(){
@@ -74,6 +92,15 @@ Template.audio_gallery.helpers({
     },
     current_song_name: function(){
         return Template.instance().current_song_name.get();
+    },
+    current_song_time: function() {
+        return Template.instance().current_song_time.get();
+    },
+    current_song_total_duration: function() {
+        return Template.instance().current_song_duration.get();
+    },
+    current_song_percentage: function() {
+        return Template.instance().current_song_percentage.get();
     }
 })
 
@@ -104,10 +131,11 @@ Template.audio_gallery.events({
         }
         else if(state=='pause'){
           state = 'resume';
-          playAudio();
+          resumeAudio();
         }
     },
     "click #button_next": function(event, template) {
+        state='play';
         current_song_id++;
         if(current_song_id > (playlist_entries.length - 1)) {
             current_song_id = 0;
@@ -116,5 +144,46 @@ Template.audio_gallery.events({
         template.current_song_name.set(playlist_entries[current_song_id].getAttribute("value"));
         Tracker.flush();
         playAudio();
+    },
+    "click #button_previous": function(event, template) {
+        current_song_id--;
+        state='play';
+        if(current_song_id < 0) {
+            current_song_id = playlist_entries.length - 1;
+        }
+        template.audio_location.set(playlist_entries[current_song_id].getAttribute("href"));
+        template.current_song_name.set(playlist_entries[current_song_id].getAttribute("value"));
+        Tracker.flush();
+        playAudio();
+    },
+    "ended audio": function(event, template) {
+        state='play';
+        current_song_id++;
+        if(current_song_id > (playlist_entries.length - 1)) {
+            current_song_id = 0;
+        }
+        template.audio_location.set(playlist_entries[current_song_id].getAttribute("href"));
+        template.current_song_name.set(playlist_entries[current_song_id].getAttribute("value"));
+        Tracker.flush();
+        playAudio();
+    },
+    "timeupdate audio": function(event, template) {
+        template.current_song_time.set(formatTime(audio.currentTime));
+        template.current_song_duration.set(formatTime(audio.duration));
+        template.current_song_percentage.set((audio.currentTime / audio.duration) * 100);
+    },
+    "click #seek_slider": function(event, template) {
+        var clicked_width = event.pageX - event.currentTarget.offsetLeft;
+        var total_element_width = event.currentTarget.clientWidth;
+        var percentage = (clicked_width / total_element_width) * 100;
+        if (percentage > 100) {
+          percentage = 100;
+        } else if (percentage < 0) {
+          percentage = 0;
+        }
+        console.log(percentage);
+        audio.currentTime = (percentage / 100) * audio.duration;
+        template.current_song_percentage.set(percentage);
+        Tracker.flush();
     }
 });
